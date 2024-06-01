a16b4217e881ebc738896d7efe7a020b9d673586
Helen <Helen@node0.cachelib2.redundancy-pg0.clemson.cloudlab.us>
2024-06-01 09:37:10 -0400

    new logs

--- examples/my_cache_1/run2.sh
+++ examples/my_cache_1/run2.sh
@@ -2,13 +2,10 @@
 
 trace_paths=()
 names=()
-suffixes=("LruTailAge" 
-	"MarginalHits"
-	"HitsPerSlab"
-	"FreeMem")
+suffixes=()
 
 if [ "$1" == "Ws" ]; then
-	prefix="/disk/CacheLib-M24/examples/my_cache_1/data/w"
+	prefix="data/w"
 	suffix=".oracleGeneral.bin.zst"
 	for i in {80..106}; do
 		trace_path="$prefix$i$suffix"
@@ -20,19 +17,35 @@ else
 	names+=("$2")	
 fi
 
+if [ "$3" == "all" ]; then
+	suffixes+=("FreeMem")
+	suffixes+=("")
+	suffixes+=("LruTailAge")
+	suffixes+=("MarginalHits")
+	suffixes+=("HitsPerSlab")
+else
+	suffixes+=("$3")	
+fi
+
+if [ -z "$4" ]; then 
+	echo "No output folder"
+	exit
+fi
+
+
 num_traces=${#trace_paths[@]}
+num_suffixes=${#suffixes[@]}
 
 # Loop through the range of $ from 80 to 106
-for (( j=0; j < 4; j++)); do
-	SUFFIX=${suffixes[j]}
-	for (( i=0; i<num_traces; i++ )); do
-    	# Define the trace file name
-    	TRACE_FILE=${trace_paths[i]}
-    	NAME=${names[i]}
+for (( i=0; i<num_suffixes; i++)); do
+	SUFFIX=${suffixes[i]}
 
-    	echo "running $TRACE_FILE with name $NAME and rebalancing strategy $SUFFIX"
-    
-    	python3 generate_output.py --tracepath="$TRACE_FILE" --name="$NAME" --suffix="$SUFFIX" --cache_size="$4"
+	for (( j=0; j<num_traces; j++ )); do
+    		# Define the trace file name
+    		TRACE_FILE=${trace_paths[j]}
+    		NAME=${names[j]}
+ 		
+		echo "$SUFFIX, $TRACE_FILE, $NAME"
+    		python3 generate_output.py --tracepath="$TRACE_FILE" --name="$NAME" --suffix="$SUFFIX" --folder="$4" --cache_size="$5" 
 	done
-done
-
+done	
